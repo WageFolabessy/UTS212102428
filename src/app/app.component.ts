90039dcc83e3d697a3f26934a1ea5e7a667e899e
WageFolabessy <mbappespsg336@gmail.com>
2023-10-28 08:58:48 +0700

    UTS selesai

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'UTS212102428';
+  nim = '212102428';
+  name = 'Endricho';
+}
---
+++ src/app/ecommerce/ecommerce.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ecommerce',
+  templateUrl: './ecommerce.component.html',
+  styleUrls: ['./ecommerce.component.css'],
+})
+export class EcommerceComponent {
+  nim = '212102428';
+  name = 'Endricho';
+  status = 'Student';
+}
